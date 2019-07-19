dc33a5718114c034a3e02e6ef26db4ce5c19dd1c
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-19 11:21:40 -0700

    updated styling

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,16 +1,34 @@
 import React from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  background: white;
+  color: violet;
+  width: 30%;
+  margin-bottom: 50px;
+`;
+
+const Heading = styled.h3`
+  color: blue;
+  font-size: 1.2rem;
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+`;
 
 const CharacterCard = props => {
   return (
-    <div>
-      <h3>{props.character.name}</h3>
+    <Wrapper>
+      <Heading>{props.character.name}</Heading>
       <p>Height: {props.character.height}</p>
       <p>Mass: {props.character.mass}</p>
       <p>Skin color: {props.character.skin_color}</p>
       <p>Eye color: {props.character.eye_color}</p>
       <p>Birth year: {props.character.birth_year}</p>
-    </div>
+    </Wrapper>
   );
 };
 
